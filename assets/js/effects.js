e3ed8723f5f0a6de4d2eafad729b6ab22e5ff60f
Andrii Zhabchyk <andrii.zhabchyk@gmail.com>
2017-07-25 02:49:10 +0300

    Add interactive effects to page

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,8 +1,19 @@
 'use strict';
 
 $(document).ready(function(){
+    fixedHeader (); //call fixed header function
+    smoothScroll (); //call smooth scroll function
+    activeNav (); //call active navigation function
+    scrollParallax (); // call parallax function home page
+    fadeServices(); //call function services fadeIn
+    portfolioHover(); //call function portfolio hover
+    aosAnimate(); //call aos animate function
+    activeClassNav();//call active navigation function
+    /*parallaxEffectContact();// call parallax function contact us*/
+});
 
-    /*Fixed header*/
+/*Fixed header function*/
+function fixedHeader () {
     $(window).scroll(function(){
         if ( $(this).scrollTop() > 30 && $("header").hasClass("default") ){
             $("header").removeClass("default").addClass("fixed");
@@ -10,13 +21,137 @@ $(document).ready(function(){
             $("header").removeClass("fixed").addClass("default");
         }
     });
+}
 
-    /*Scroll*/
+/*Smooth scroll function*/
+function smoothScroll () {
     $('a[href^="#"]').bind("click", function(e){
-        var anchor = $(this);
+        let anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $(anchor.attr('href')).offset().top
-        }, 1000);
+        }, 700);
         e.preventDefault();
+    });   
+}
+
+//active class to navigation
+function activeNav() {
+    $('li > a').click(function () {
+        $('a').removeClass('active');
+        $(event.currentTarget).addClass('active');
     });
-});
+}
+
+//parallax function
+function scrollParallax () {
+    $(window).scroll( function () {
+        let scroll = $(this).scrollTop();
+        let scrollHomeScreen = $('.home > h3, h1, .btn');
+
+        $(scrollHomeScreen).css(
+            `transform`, `translate(0%, ${scroll/3}%)`
+        );
+
+        if ($(this).scrollTop() > 700) {
+            $(scrollHomeScreen).css(
+                `transform`, `translate(0%, 0%)`
+            )
+        }
+    });
+}
+
+//functions services fadeIn
+function fadeServices() {
+    $(window).scroll( function () {
+        if ($(this).scrollTop() === $('#services').offset().top){   //offset by elem from start page
+            anime({
+                targets: '.services',
+                opacity: 0
+            });
+
+            let relativeOffset = anime.timeline(); //anime duration timeline
+
+            relativeOffset
+                .add({
+                    targets: '.responsive',
+                    translateX: [- 200, 0],
+                    easing: 'linear',
+                    opacity: [0, 1],
+                    duration: 300
+                })
+                .add({
+                    targets: '.security',
+                    translateX: [- 400, 0],
+                    easing: 'linear',
+                    opacity: [0, 1],
+                    duration: 400
+                })
+                .add({
+                    targets: '.services',
+                    translateY: [-50, 0],
+                    opacity: [0, 1],
+                    offset: '-=100',
+                    duration: 500
+                });
+        }
+    });
+}
+
+//hover effect to item portfolio cell
+function portfolioHover() {
+    $('.hover').mouseover(function () {
+        $(event.currentTarget).siblings('.p-item-footer').addClass('hoverText');
+    });
+    $('.hover').mouseleave(function () {
+        $(event.currentTarget).siblings('.p-item-footer').removeClass('hoverText');
+    });
+}
+
+//aos jquery plugin animate
+function aosAnimate() {
+    AOS.init({
+        offset: 200,
+        duration: 450,
+        easing: 'ease-in-sine',
+        delay: 100,
+    });
+}
+
+//active class to navigation
+function activeClassNav() {
+    $(window).scroll( function () {
+        let scroll = $(this).scrollTop();
+        $('a').removeClass('active');
+
+        if (scroll >= $('#home').offset().top -100 && scroll < $('#services').offset().top - 100) {
+            $('li > a[href="#home"]').addClass('active');
+        }
+        if (scroll > $('#services').offset().top -100 && scroll < $('#portfolio').offset().top - 100) {
+            $('li > a[href="#services"]').addClass('active');
+        }
+        if (scroll > $('#portfolio').offset().top -100 && scroll < $('#about').offset().top - 100) {
+            $('li > a[href="#portfolio"]').addClass('active');
+        }
+        if (scroll > $('#about').offset().top -100 && scroll < $('#contact').offset().top - 100) {
+            $('li > a[href="#about"]').addClass('active');
+        }
+        if (scroll > $('#contact').offset().top -100) {
+            $('li > a[href="#contact"]').addClass('active');
+        }
+    });
+}
+
+/*
+function parallaxEffectContact() {
+    $(window).scroll( function () {
+        let scroll = $(this).scrollTop();
+        let scrollContactScreen = $('#contact > .container');
+        console.log(scroll);
+
+        $(scrollContactScreen).css(
+            `transform`, `translate(0%, ${-scroll/120}%)`
+        )
+    });
+}*/
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,36 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
+'use strict';
+
+let gulp = require('gulp'),
+    minifyjs = require('gulp-js-minify'),
     watch = require('gulp-watch'),
     livereload = require('gulp-livereload'),
     less = require('gulp-less'),
     csso = require('gulp-csso'),
     connect = require('gulp-connect');
 
+//compilation less to css
 gulp.task('less-to-css', function () {
     gulp.src('assets/less/*.less')
         .pipe(less())
+        .pipe(csso())
         .pipe(gulp.dest('dist/style'))
         .pipe(connect.reload());
 });
 
-gulp.task('watch',function(){
+//file to change & reload window
+gulp.task('watch', function(){
     gulp.watch('assets/less/*.less', ['less-to-css']);
+    gulp.watch('assets/js/*.js', ['min-js']);
 });
 
+gulp.task('min-js', function () {
+    return gulp.src(['assets/js/*.js', '!/node_modules/**'])
+        .pipe(minifyjs())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(connect.reload());
+});
+
+//start to connect with local server
 gulp.task('connect', function() {
     connect.server({
         livereload: true
