2736fc94c7fe78aac6270cae2af3cbe44f759d26
Andrii Zhabchyk <andrii.zhabchyk@gmail.com>
2017-07-30 16:53:17 +0300

    Add libs and fix responsive & header

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,45 @@
 'use strict';
 
 let gulp = require('gulp'),
-    minifyjs = require('gulp-js-minify'),
     watch = require('gulp-watch'),
     livereload = require('gulp-livereload'),
     less = require('gulp-less'),
     csso = require('gulp-csso'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    jsmin = require('gulp-minify');
 
 //compilation less to css
 gulp.task('less-to-css', function () {
-    gulp.src('assets/less/*.less')
+    return gulp.src('assets/less/*.less')
         .pipe(less())
         .pipe(csso())
-        .pipe(gulp.dest('dist/style'))
+        .pipe(gulp.dest('assets/css'));
+});
+
+//
+gulp.task('concatCSS', function () {
+    return gulp.src([
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/aos/dist/aos.css',
+        'assets/css/style.css'
+    ])
+        .pipe(concat('build.min.css'))
+        .pipe(gulp.dest('build/style'))
         .pipe(connect.reload());
 });
 
 //file to change & reload window
 gulp.task('watch', function(){
     gulp.watch('assets/less/*.less', ['less-to-css']);
+    gulp.watch('build/style/style.css', ['concatCSS']);
     gulp.watch('assets/js/*.js', ['min-js']);
 });
 
 gulp.task('min-js', function () {
-    return gulp.src(['assets/js/*.js', '!/node_modules/**'])
-        .pipe(minifyjs())
-        .pipe(gulp.dest('dist/js'))
+    return gulp.src('assets/js/effects.js')
+        .pipe(jsmin())
+        .pipe(gulp.dest('build/js'))
         .pipe(connect.reload());
 });
 
